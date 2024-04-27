dd96a6dcfda51a77ec8b44910b4a72f05dcb621f
Wambugu Joseph <116286199+codetheuri@users.noreply.github.com>
2024-04-27 14:31:55 +0300

    Update shared.js

--- maarifa/started/resources/shared.js
+++ maarifa/started/resources/shared.js
@@ -5,18 +5,11 @@ import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
 
 import { marked } from "https://esm.run/marked";
 const { HarmCategory, HarmBlockThreshold } = GoogleGenerativeAI;
-/**
 
- *
- * @param {GoogleGenerativeAI.ModelParams} params
- * @returns {GoogleGenerativeAI.GenerativeModel}
- */
 export async function getGenerativeModel(params) {
-  // Fetch API key from server
-  // If you need a new API key, get it from https://makersuite.google.com/app/apikey
 
 
-  const API_KEY = "AIzaSyA54EJaKJ_zW4dsnwMWACMDvJT3UaF5zjQ";
+  const API_KEY = "Your api key";
   const MODEL_NAME = "latest";
 
   const genAI = new GoogleGenerativeAI(API_KEY);
@@ -29,7 +22,7 @@ export async function getGenerativeModel(params) {
     {text: "output: my name is Joseph Theuri "},
   ];
   
-  // For text-only inputs, use the gemini-pro model
+
   return genAI.getGenerativeModel(params);
 }
 async function run() {
@@ -42,14 +35,7 @@ async function run() {
     maxOutputTokens: 8192,
   };
 }
-/**
- * Converts a File object to a GoogleGenerativeAI.Part object.
- *
- * @param {Blob} file
- * @returns {GoogleGenerativeAI.Part}
- */
 
-;
 export async function fileToGenerativePart(file) {
   const base64EncodedDataPromise = new Promise((resolve) => {
     const reader = new FileReader();
@@ -61,22 +47,13 @@ export async function fileToGenerativePart(file) {
   };
 }
 
-/**
- * Scrolls the document all the way to the bottom.
- */
+
 export function scrollToDocumentBottom() {
   const scrollingElement = document.scrollingElement || document.body;
   scrollingElement.scrollTop = scrollingElement.scrollHeight;
 }
 
-/**
- * Updates the `resultEl` with parsed markdown text returned by a `getResult()` call.
- *
- * @param {HTMLElement}} resultEl
- * @param {() => Promise<GoogleGenerativeAI.GenerateContentResponse>} getResult
- * @param {boolean} streaming
- * 
- */
+
 const generationConfig = {
   temperature: 1,
   topK: 0,
@@ -90,17 +67,13 @@ export async function updateUI(resultEl, getResult, streaming,model,parts,genera
   resultEl.className = "loading";
   let text = "";
   try {
-    // const result = await model.getResult({
-    //   contents: [{ role: "user", parts }],
-     
-    // });
+  
    const result= await getResult();
 
 
     if (streaming) {
       resultEl.innerText = "";
       for await (const chunk of result.stream) {
-        // Get first candidate's current text chunk
         const chunkText = chunk.text();
         text += chunkText;
         resultEl.innerHTML = marked.parse(text);
@@ -111,7 +84,7 @@ export async function updateUI(resultEl, getResult, streaming,model,parts,genera
       text = response.text();
     }
 
-    resultEl.className = ""; // Remove .loading class
+    resultEl.className = ""; 
   } catch (err) {
     text += "\n\n> " + err;
     resultEl.className = "error";
